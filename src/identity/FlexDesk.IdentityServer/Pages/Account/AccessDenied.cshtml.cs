2fce60e5a4d0703e74c0ceb11e26580e75fb3871
alim_al-zakaria <alzakariaalim@gmail.com>
2026-04-27 14:54:04 +0300

    Add Identity Server project. now it is the satndart identity server with asp net core identity template

---
+++ src/identity/FlexDesk.IdentityServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlexDesk.IdentityServer.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
